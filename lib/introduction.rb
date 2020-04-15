32020cb173fbbf42974e580aee2eeacdaf24418d
Anmol Kaur <kaur.anmol26@gmail.com>
2020-04-15 20:46:09 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(a, b)
+  puts "Hi, my name is #{a} and I am learning to program in #{b}."
+end
+
+introduction_with_language("Dan", "Ember.js")

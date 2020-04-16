32ee661e2ad5796e4c21702ed9663abbacd54ae6
Anmol Kaur <kaur.anmol26@gmail.com>
2020-04-16 07:29:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,8 +2,8 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(a, b)
-  puts "Hi, my name is #{a} and I am learning to program in #{b}."
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 introduction_with_language("Dan", "Ember.js")
